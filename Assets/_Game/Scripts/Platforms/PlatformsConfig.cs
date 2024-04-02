6cbd201f2eceb5e0032eeafcdbf94e5095019fcb
JanaAlBdourTT <janaa@tabtale.com>
2024-04-02 14:22:27 +0300

    Platform pooling

--- Assets/_Game/Scripts/Platforms/PlatformsConfig.cs
+++ Assets/_Game/Scripts/Platforms/PlatformsConfig.cs
@@ -9,6 +9,7 @@ namespace Scripts.Platforms
 		public PlatformView[] platformPrefabs;
 		public Quaternion     rotation;
 		public int            startCount        = 15;
+		public int            poolInitialCount  = 20;
 		public Vector3        minDistance       = new Vector3( 2, -2 );
 		public Vector3        maxDistance       = new Vector3( 8, 2 );
 		public float          disappearDistance = 10;
--- Assets/_Game/Scripts/Platforms/PlatformsService.cs
+++ Assets/_Game/Scripts/Platforms/PlatformsService.cs
@@ -6,6 +6,7 @@ using Scripts.Extensions;
 using Scripts.Player;
 using Scripts.PlayerLoop;
 using UnityEngine;
+using UnityEngine.Pool;
 using UnityEngine.Profiling;
 using Zenject;
 using Object = UnityEngine.Object;
@@ -63,9 +64,11 @@ namespace Scripts.Platforms
 			return closestPlatform;
 		}
 
-		private PlatformsConfig  _config;
-		private IDistanceService _distanceService;
-		private IPlayerService   _playerService;
+		private PlatformsConfig          _config;
+		private IDistanceService         _distanceService;
+		private IPlayerService           _playerService;
+		private PlatformView             _startPlatform;
+		private ObjectPool<PlatformView> _pool;
 
 		[Inject]
 		private void Construct( PlatformsConfig config, IDistanceService distanceService, IPlayerService playerService, IPlayerLoopService playerLoopService )
@@ -75,53 +78,25 @@ namespace Scripts.Platforms
 			_distanceService = distanceService;
 			_playerService   = playerService;
 			
+			_startPlatform = Object.Instantiate( _config.startPlatformPrefab, Vector3.zero, _config.rotation );
+
+			_pool = new ObjectPool<PlatformView>( CreatePlatform, GetPlatform, ReleasePlatform, DestroyPlatform, false, _config.poolInitialCount, _config.poolInitialCount );
+			
 			playerLoopService.OnStarted    += DestroyAndSpawn;
 			playerLoopService.OnUpdateTick += HandleDisappearingAndRespawning;
 		}
 
-		private void DestroyAndSpawn( )
+		private PlatformView CreatePlatform( )
 		{
-			Profiler.BeginSample( "Initial Platform Spawning" );
-			foreach ( var platform in Platforms )
-			{
-				Object.Destroy( platform.gameObjectCached );
-			}
-
-			Platforms.Clear( );
-			OnResetPlatforms.Invoke( );
-
-			var startPlatform = Object.Instantiate( _config.startPlatformPrefab, Vector3.zero, _config.rotation );
-			Platforms.Add( startPlatform );
-
-			for ( var index = 0; index < _config.startCount; index++ )
-			{
-				SpawnPlatform( );
-			}
-
-			Profiler.EndSample( );
-		}
-
-		private void HandleDisappearingAndRespawning( )
-		{
-			foreach ( var platform in Platforms.ToArray( ) )
-			{
-				var distance = _playerService.Player.transformCached.position.x - platform.transformCached.position.x;
-				if ( distance > _config.disappearDistance )
-				{
-					OnDestroyPlatform.Invoke( platform );
-					Platforms.Remove( platform );
-					Object.Destroy( platform.gameObjectCached );
-
-					SpawnPlatform( );
-				}
-			}
+			var platform = Object.Instantiate( _config.platformPrefabs.GetRandomElement( ) );
+			return platform;
 		}
 		
-		private void SpawnPlatform( )
+		private void GetPlatform( PlatformView platform )
 		{
+			platform.gameObjectCached.SetActive( true );
+			
 			var lastPlatform = Platforms[^1];
-
-			var platform = Object.Instantiate( _config.platformPrefabs.GetRandomElement( ) );
 
 			var position = lastPlatform.transformCached.position;
 				
@@ -135,9 +110,57 @@ namespace Scripts.Platforms
 				
 			platform.transformCached.position = position;
 			platform.transformCached.rotation = _config.rotation;
-
+			
 			Platforms.Add( platform );
 			OnSpawnPlatform.Invoke( platform );
+		}
+		
+		private void ReleasePlatform( PlatformView platform )
+		{
+			platform.gameObjectCached.SetActive( false );
+		}
+		
+		private void DestroyPlatform( PlatformView platform )
+		{
+			Platforms.Remove( platform );
+			OnDestroyPlatform.Invoke( platform );
+		}
+
+		private void DestroyAndSpawn( )
+		{
+			foreach ( var platform in Platforms )
+			{
+				_pool.Release( platform );
+			}
+
+			Platforms.Clear( );
+			OnResetPlatforms.Invoke( );
+			
+			Platforms.Add( _startPlatform );
+			_startPlatform.gameObjectCached.SetActive( true );
+
+			for ( var index = 0; index < _config.startCount; index++ )
+			{
+				_pool.Get( );
+			}
+		}
+
+		private void HandleDisappearingAndRespawning( )
+		{
+			var platformsArray = Platforms.ToArray( );
+			for ( var index = 1; index < platformsArray.Length; index++ )
+			{
+				var platform = platformsArray[index];
+				var distance = _playerService.Player.transformCached.position.x - platform.transformCached.position.x;
+				if ( distance > _config.disappearDistance )
+				{
+					_pool.Release( platform );
+					Platforms.Remove( platform );
+					OnDestroyPlatform.Invoke( platform );
+
+					_pool.Get( );
+				}
+			}
 		}
 	}
 }
